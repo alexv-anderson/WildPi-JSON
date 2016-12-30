cf681ba015fd596dfa21e8ea1d860916659e2a28
alexv-anderson <opiemike613@yahoo.com>
2016-12-29 20:19:35 -0600

    Fix to test helper

--- test/json/TestHelper.java
+++ test/json/TestHelper.java
@@ -2,14 +2,13 @@ package json;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.CoreMatchers.not;
-import static org.hamcrest.CoreMatchers.notNullValue;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class TestHelper
 {
     public static <T> void equalityAndHashcodeChecker(T same1, T same2, T different)
     {
-        assertThat("Objects should not be equal to null", same1, is(notNullValue()));
+        assertThat("Objects should not be equal to null", !same1.equals(null));
         assertThat("Object of different types should not be equal", same1, is(not(new Object())));
         assertThat("An object should be equal to itself", same1, is(same1));
         assertThat("Equal objects should be equal", same1, is(same2));
